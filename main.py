070c76635c028956ef6e6f8ca666ce33e872bdb1
kidmpukane <lwandisozwane91@gmail.com>
2025-11-11 12:31:28 +0200

    Refactor anomaly detection call in main.py to use user intent instead of hardcoded classification

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ from core.intent_layer.machine_modules import SolarPanelFDIRModule
 from core.input_layer.input_layer import capture_diagnostic_request
 from data.machines.machine_profiles import machine_profile, fault_mapping, telemetry
 
-intent_classification = {
-    "Asset_ID": "PV_PANEL_09SP823",
-    "Machine_Type": "Solar_Panel",
-    "Classified_Intent": "Connection_Monitoring",
-    "Identified_Metric": "Voltage_V",
-    "Time_Window_Hrs": 12,
-}
 
+# Enter ID:s PV_PANEL_09SP823
 
-# print(capture_diagnostic_request())
+user_intent = capture_diagnostic_request()
 fdir_module = SolarPanelFDIRModule()
-print(fdir_module.anomaly_detection(
-    intent_classification, telemetry, machine_profile, fault_mapping))
+stage_1 = fdir_module.anomaly_detection(
+    user_intent, telemetry, machine_profile, fault_mapping)
+print(stage_1)
