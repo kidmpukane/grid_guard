5994aabf6de46ae43b237774a2d52a77e338f7ad
kidmpukane <lwandisozwane91@gmail.com>
2025-11-11 09:48:57 +0200

    Refactor telemetry and fault mapping logic for Solar Panel module; remove mock data from machine_modules.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from core.input_layer.input_layer import capture_diagnostic_request
+from data.machines.machine_profiles import machine_profile, fault_mapping
 
-print(capture_diagnostic_request())
+intent_classification = {
+    "Asset_ID": "PV_PANEL_09SP823",
+    "Machine_Type": "Solar_Panel",
+    "Classified_Intent": "Connection_Monitoring",
+    "Identified_Metric": "Voltage_V",
+    "Time_Window_Hrs": 12,
+}
+
+print(machine_profile, fault_mapping)
+
+
+# print(capture_diagnostic_request())
+# fdir_module = SolarPanelFDIRModule()
+# anomaly_report = fdir_module.anomaly_detection(
+#     intent_classification, telemetry, machine_profile, fault_mapping)
+
+# print(anomaly_report(intent_classification,
+#       telemetry, machine_profile, fault_mapping))
